5b989c981c2828bd15dc5b721a9bcc7856a4d1ac
LeeJongHun <whdcn@naver.com>
2016-03-28 10:22:08 +0900

    Add Bottom Sheet Callback Add Bottom Sheet Dialog Fragment

--- app/src/main/java/chummong/io/testbottomsheet/MainActivity.java
+++ app/src/main/java/chummong/io/testbottomsheet/MainActivity.java
@@ -1,7 +1,9 @@
 package chummong.io.testbottomsheet;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.BottomSheetDialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
@@ -20,6 +22,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         findViewById(R.id.button_03).setOnClickListener(this);
 
         bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
+        bottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                if (newState == BottomSheetBehavior.STATE_COLLAPSED) {
+                    bottomSheetBehavior.setPeekHeight(0);
+                }
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+
+            }
+        });
     }
 
     @Override
@@ -28,7 +43,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.button_01:
                 bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
                 break;
-
+            case R.id.button_02:
+                bottomSheetBehavior.setPeekHeight(300);
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                break;
+            case R.id.button_03:
+                BottomSheetDialogFragment fragment = new TutsPlusBottomSheetDialogFragment();
+                fragment.show(getSupportFragmentManager(), fragment.getTag());
+                break;
         }
     }
 }
